0cccca72466807241335844e4c2df21abac44fba
ananyyaa-22 <ananyas.1331@gmail.com>
2026-04-19 19:28:58 +0530

    assignment-6

--- Assignment-6/ques1.cpp
+++ Assignment-6/ques1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
+
 class Book
 {
 public:
@@ -8,41 +9,96 @@ public:
   string author;
   int isbn;
 
-  void setDetails(string &title, string &author, int &isbn)
-  {
-    this->title = title;
-    this->author = author;
-    this->isbn = isbn;
-  }
-
-  void display()
-  {
-    cout << "Title: " << title << endl;
-    cout << "Author: " << author << endl;
-    cout << "ISBN: " << isbn << endl;
-  }
+  void setDetails(string &title, string &author, int &isbn);
+  void display();
 };
+
+void Book::setDetails(string &title, string &author, int &isbn)
+{
+  this->title = title;
+  this->author = author;
+  this->isbn = isbn;
+}
+
+void Book::display()
+{
+  cout << "Title: " << this->title << endl;
+  cout << "Author: " << this->author << endl;
+  cout << "ISBN: " << this->isbn << endl;
+}
+
 class Library
 {
-  public:
+public:
   Book books[100];
   static int count;
 
-    bool addNewBook(string &title, string &author, int &isbn){
-    if(count>=100){
-      return false;
-    }
-    books[count].setDetails(title, author, isbn);
-    count++;
-    return true; 
-  }
-  
-    bool  removeBooks(string &isbn);
-    void displayDetails();
+  bool addNewBook(string &title, string &author, int &isbn);
+  bool removeBooks(int isbn);
+  void displayDetails();
 };
-int Library::count;
-bool Library::removeBooks(string &isbn){
-  for(int i=0; i<count; i++){
-    
+
+int Library::count = 0;
+
+bool Library::addNewBook(string &title, string &author, int &isbn)
+{
+  if (count >= 100)
+  {
+    return false;
   }
+  books[count].setDetails(title, author, isbn);
+  count++;
+  return true;
+}
+
+bool Library::removeBooks(int isbn)
+{
+  for (int i = 0; i < count; i++)
+  {
+    if (books[i].isbn == isbn)
+    {
+      for (int j = i; j < count - 1; j++)
+      {
+        books[j] = books[j + 1];
+      }
+      count--;
+      return true;
+    }
+  }
+  return false;
+}
+
+void Library::displayDetails()
+{
+  for (int i = 0; i < count; i++)
+  {
+    books[i].display();
+    cout << endl;
+  }
+}
+
+int main()
+{
+  Library lib;
+
+  string t1 = "C++";
+  string a1 = "Bjarne";
+  int i1 = 101;
+
+  string t2 = "DSA";
+  string a2 = "Mark";
+  int i2 = 102;
+
+  lib.addNewBook(t1, a1, i1);
+  lib.addNewBook(t2, a2, i2);
+
+  cout << "Books in Library:\n";
+  lib.displayDetails();
+
+  lib.removeBooks(101);
+
+  cout << "\nAfter Removal:\n";
+  lib.displayDetails();
+
+  return 0;
 }
